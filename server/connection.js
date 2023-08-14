a9d47f8bce5d79df1119431f57abdf2289c61b62
Nurullah Al Noor <nurullah408@gmail.com>
2023-08-14 10:10:35 +0600

    Co-authored-by: syedateef <syedateef@users.noreply.github.com>

---
+++ server/connection.js
@@ -0,0 +1,15 @@
+//establishing the mysql connection
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'Nurullah',
+    password: 'nuru11@hA1N00r',
+    database: 'lankabangla'
+})
+
+//testing the connection 
+
+module.exports = db;
+
+
