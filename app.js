70985bc450a7d9ea999c467c65603a4e36517d25
Nurullah Al Noor <nurullah408@gmail.com>
2023-08-15 19:43:52 +0600

    deleted body-parser as it does not allow multipart

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 // ---Boiler plate code---
 // The express framework is brought in
 const express = require('express');
-//body parser package
-var bodyParser = require('body-parser');
+//multer package to handle multipart form data
+const multer = require('multer');
+const upload = multer();
 // The port number the app will listen on
 const PORT = 5500
 // 'app' is an instance of express()
 const app = express();
-//These settings must be given for json transfers
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+
 const path = require('path');
 //Setting the templating engine 
 app.set('view engine','ejs');
@@ -24,11 +23,13 @@ const clientForm = require('./routes/clientFormRoute');
 const aboutUs = require('./routes/aboutUsRoute');
 const logIn = require('./routes/loginRoute');
 const index = require('./routes/homeRoute');
+const adminDash = require('./routes/adminRoute');
 
 
 app.get('/',(req,res) => {
     res.render('index')
 })
+
 
 app.use('/dashboard',dashboard);
 app.use('/form',BoForm);
@@ -36,7 +37,10 @@ app.use('/clientForm',clientForm);
 app.use('/AboutUs',aboutUs);
 app.use('/login',logIn);
 app.use('/index',index)
+app.use('/adminDash',adminDash)
 
+//100009823
+//621230928429322
 //app using main route at localhost:5000/
 app.listen(PORT,(req,res) => {
     console.log(`App listening on ${PORT}`);
