bfcd052054dafdaed3c20157c6c620602086ab23
Nurullah Al Noor <nurullah408@gmail.com>
2023-08-15 23:46:14 +0600

    fixed data entry

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -5,12 +5,12 @@ const adminRoute = express.Router();
 
 const db = require('../server/connection');
 
-adminRoute.use('/',(req,res) => {
+adminRoute.get('/',(req,res) => {
+    console.log("Its getting it now.");
     res.render('adminDash');
 })
 
-const sqlQuery = 'SELECT * FROM lankabangla.client';
-db.query(sqlQuery, (error, results) => {
+/* db.query(sqlQuery, (error, results) => {
     if (error) {
         console.error('Error querying the database:', error);
         return;
@@ -19,31 +19,27 @@ db.query(sqlQuery, (error, results) => {
     // Pass the results to the EJS template for rendering
     // Render the template and pass the data
     adminRoute.get('/', (req, res) => {
-        res.render('adminDash', { results: results });
+        res.render('adminDash');
     });
-});
+}); */
+//bank account_number : 100009173
+//bo_account_number : 620232349173665
+//
 
 //Add the sql queries here
 adminRoute.post('/', upload.none(), (req,res) => {
     const {bankAccountNum, clientBoNum} = req.body;
     bank_sql = 'INSERT INTO lankabangla.bank (account) VALUES (?)';
-    /*db.query(bank_sql,[bankAccountNum],(err,results) => {
+    db.query(bank_sql,[bankAccountNum],(err,results) => {
         if (err) throw err;
-        res.json({message: 'bank account added.'});
-    });*/
-    bo_sql = `INSERT INTO lankabangla.bo_account (bo_number) VALUES (?)`;
-    /*db.query(bo_sql,[clientBoNum],(err,results) => {
-        if(err){
-            res.render('adminDash',{text: "Entry Failed" || ''});
-        } else {
-            res.render('adminDash',{text:"Entry Success" || ''});
-        }
+        console.log(bankAccountNum)
+    });
+    /*bo_sql = `INSERT INTO lankabangla.bo_account (bo_number) VALUES (?)`;
+    db.query(bo_sql,[clientBoNum],(err,results) => {
+        if(err)throw err;
+        console.log(clientBoNum);
     });*/
 });
-adminRoute.put('/',upload.none(), (req,res) => {
-
-})
-
 
 
 
